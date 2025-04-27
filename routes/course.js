3461b2e1c5883fc70b817459c134644e2661aaad
Yash Namdeo <yashnamdeo754@gmail.com>
2025-04-27 11:58:17 +0530

    completed the courses routes

--- routes/course.js
+++ routes/course.js
@@ -1,16 +1,31 @@
-const { Router } = require('express');
+const { Router } = require("express");
+const { userMiddleware } = require("../middleware/user");
+const { purchaseModel, courseModel } = require("../db")
 const courseRouter = Router();
 
-courseRouter.post('/purchase', (req,res) =>{
+courseRouter.post("/purchase", userMiddleware, async function(req, res) {
+    const userId = req.userId;
+    const courseId = req.body.courseId;
+
+    // should check that the user has actually paid the price
+    await purchaseModel.create({
+        userId,
+        courseId
+    })
+
     res.json({
-        message: 'signup endpoint'
-    });
-});
-courseRouter.get('/preview', (req,res) =>{
+        message: "You have successfully bought the course"
+    })
+})
+
+courseRouter.get("/preview", async function(req, res) {
+    
+    const courses = await courseModel.find({});
+
     res.json({
-        message: 'signup endpoint'
-    });
-});
+        courses
+    })
+})
 
 module.exports = {
     courseRouter: courseRouter
